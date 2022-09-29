f81841a490fea517600d3905eaaf5d6bfe737593
Anoop Tatti <anoop.tatti@gmail.com>
2022-09-29 14:44:52 +0100

    feat: code to generate JSON

---
+++ resources/Generate-Predictions.ps1
@@ -0,0 +1,60 @@
+try {
+    $json = @();
+    # get all files in the srcfiles folder
+    $files = Get-ChildItem -Path ".\spopsdocs" -Filter "*.md" -Recurse;
+
+    # loop through each file
+    $files | ForEach-Object {
+    
+        # get file name without extension
+        $baseName = $_.BaseName.ToLower();
+
+        # get the file data
+        $fileData = Get-Content $_.FullName -Raw;
+        # create a regex pattern to match the example code
+    
+        $pattern = "(?s)(?<=``````powershell)(.*?)(?=``````)"
+        $options = [Text.RegularExpressions.RegexOptions]'IgnoreCase, CultureInvariant';
+    
+        $result = [regex]::Matches($fileData, $pattern, $options);
+
+        $i = 1;
+        foreach ($item in $result) {
+
+            $value = $item.Value.Trim();
+
+            # if the item value contains [ then don't add it to the json
+            if ($value -match "\[") {
+                continue;
+            }
+
+            # remove every thing after the first \n
+            $value = $value.Split("`n")[0];
+
+            # match multiple spaces and replace with a single space
+            $value = $value -replace "\s+", " ";
+
+            # remove `
+            $value = $value -replace "`` ", "";
+
+            # if the item value begins with the name of the file then add it to the json
+            if ($value.ToLower() -match "^$($baseName).*") {
+
+                $json += @{
+                    "Command" = $value
+                    "Rank"    = $i
+                }
+                $i++;
+            }
+        }
+    }
+
+
+    # write the json to a file
+    $json | ConvertTo-Json -Depth 10 | Out-File -FilePath ".\SPO.PowerShell.Suggestions.json" -Encoding UTF8 -Force;   
+}
+catch {
+    Write-Error "Unable to create prediction commands file";
+    Write-Error $_.Exception.Message;
+    exit 1;
+}
