4eaa22998b0928d2324112e4034704f7160e1382
Anoop Tatti <anoop.tatti@gmail.com>
2022-09-30 02:14:36 +0100

    feat: workflow test

--- resources/Generate-LivePredictions.ps1
+++ resources/Generate-LivePredictions.ps1
@@ -58,13 +58,8 @@ try {
         "Suggestions"   = $suggestions
     }
 
-    # check if file named SPO.PowerShell.Suggestions.live.json exists if not create it
-    if (!(Test-Path -Path "SPO.PowerShell.Suggestions.live.json")) {
-        New-Item -Path "SPO.PowerShell.Suggestions.live.json" -ItemType File -Force
-    }
-
     # write the json to a file
-    $json | ConvertTo-Json -Depth 10 | Out-File -FilePath ".\SPO.PowerShell.Suggestions.live.json" -Encoding UTF8 -Force;
+    $json | ConvertTo-Json -Depth 10 | Out-File -FilePath ".\resources\SPO.PowerShell.Suggestions.live.json" -Encoding UTF8 -Force;
 
     # Set-content to the new json object
     # $json | ConvertTo-Json -Depth 10 | Set-Content -Path "SPO.PowerShell.Suggestions.live.json" -Encoding utf8 -Force
